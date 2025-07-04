e8f9446dbceb46e590bf460d15953b56936f17de
Y0U <rubalsharma507@gmail.com>
2025-07-04 21:51:11 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+echo Setup Starting........
+echo ""
+sudo gem install net-ping
+echo ""
+sudo gem install colorize
+echo ""
+sudo gem install optparse
+echo ""
+sudo gem install  timeout
+echo ""
+sudo gem install socket
+echo""
+
+echo Setup Complete GO AHEAD!!!!!!
